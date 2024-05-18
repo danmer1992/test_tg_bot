757cb999e70df6ac068eeaad757696479772ed81
danmer1992 <danmer2991@gmail.com>
2024-05-18 15:45:19 +0300

    version 0.5

--- weather_bot.py
+++ weather_bot.py
@@ -25,11 +25,13 @@ def request_of_weather(message):
 @bot.message_handler(content_types=['text'])
 def answer(message):
     town = message.text.strip().capitalize()
-
     result_weather = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={translate(town)}&appid={API_KEY_WEATHER}&lang=RU&units=metric")
     if result_weather.status_code == 200:
         bot.reply_to(message,f"Температура: {int(result_weather.json()['main']['temp'])}\nСостояние: {result_weather.json()['weather'][0]['description']}")
+        win = bot.send_message(message.chat.id, f"{message.from_user.id}")
+        bot.register_next_step_handler(win, answer)
     else:
-        bot.reply_to(message, "Такой город не существует либо вы ввели неправильное название, попробуйте еще раз")
+        lose = bot.send_message(message.chat.id,f"Такого города не существует {message.from_user.first_name},\nлибо вы неправильно написали название города.")
+        bot.register_next_step_handler(lose, answer)
 
 bot.polling(none_stop=True)
